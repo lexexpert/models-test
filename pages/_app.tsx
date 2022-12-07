b27ad7a33be9658241d0cf307b511f4915944e04
Ivan <ivanreshetar@gmail.com>
2022-12-06 22:24:53 -0500

    Update deployment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,19 +2,8 @@ import 'styles/global.scss';
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import ThemeProvider from 'theme/ThemeProvider';
-import { useEffect, useState } from 'react';
 
 function App({ Component, pageProps }: AppProps) {
-  // const [showChild, setShowChild] = useState(false);
-
-  // useEffect(() => {
-  //   setShowChild(true);
-  // }, []);
-
-  // if (!showChild) {
-  //   return null;
-  // }
-
   return (
     <ThemeProvider>
       <Component {...pageProps} />
