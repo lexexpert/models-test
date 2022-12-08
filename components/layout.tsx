1cec5714d1a8fc07a87628d0b0c7ecbf9e6c7691
Ivan <ivanreshetar@gmail.com>
2022-12-08 09:19:47 -0500

    Update translation

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,17 +6,19 @@ import Stack from '@mui/material/Stack';
 import Container from '@mui/material/Container';
 import Footer from './footer';
 import Box from '@mui/material/Box';
+import { useTranslation } from 'next-i18next';
 
 interface LayoutPropsType {
   children: ReactChild;
 }
 
 export default function Layout({ children }: LayoutPropsType) {
+  const { t } = useTranslation();
   return (
     <Stack sx={{ height: '100%' }}>
       <Head>
         <title>{mainConfig.appName}</title>
-        <meta name="description" content="OnlyModels description" />
+        <meta name="description" content={t('app_description')} />
         <meta property="og:image" content={`favicon.ico`} />
         <meta name="og:title" content={mainConfig.appName} />
       </Head>
